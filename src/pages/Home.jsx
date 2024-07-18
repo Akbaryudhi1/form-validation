6cf8d87bb6b190446f59ad5d7f91c91cba26073a
Akbaryudhi1 <yudhisj01@gmail.com>
2024-07-18 23:31:28 +0700

    tugas

---
+++ src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import LoginPage from "../components/LoginPage";
+
+const Home = () => {
+  return (
+    <div>
+      <LoginPage />{" "}
+    </div>
+  );
+};
+
+export default Home;
